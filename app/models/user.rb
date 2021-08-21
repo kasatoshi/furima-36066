4dfc3b9cb0e1d3623bf506b581220b3c38b6f69e
kasatoshi <gold_experience_3104@yahoo.co.jp>
2021-08-21 21:46:32 +0900

    新規登録からログインまで　4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6}\z/
+  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])\w+\z/
     validates :password, presence: true,
-            format: { with: VALID_PASSWORD_REGEX,}
+            format: { with: VALID_PASSWORD_REGEX}
 
    with_options presence: true do
     validates :nickname
     validates :birthday
-     with_options { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-      validates :last_name format: 
-      validates :first_name format: 
+     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
    end
-    with_options { with: /\A[ァ-ヶ]+\z/} do
-      validates :last_name_kana format: { with: /\A[ァ-ヶ]+\z/}
-      validates :first_name_kana format: { with: /\A[ァ-ヶ]+\z/}
+
+    with_options format: { with: /\A[ァ-ヶ]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
     end
   end
 end
