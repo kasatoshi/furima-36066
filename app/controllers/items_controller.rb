18c384de844f2da989ebdd133f85f14d18be8490
kasatoshi <gold_experience_3104@yahoo.co.jp>
2021-10-01 16:29:55 +0900

    Revert "Merge pull request #17 from kasatoshi/商品情報編集機能"
    This reverts commit 268de722adba01e95b5fee65da1df71c99c4da66, reversing
    changes made to 8a71d610e9fd3982a466d41ee88e9ce07075902d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:create, :show, :index]
-  before_action :set_item, only: [:show]
+  before_action :authenticate_user!, except: [:create, :show, :index, :edit, :destroy]
   def new
     @item = Item.new
   end
@@ -16,15 +15,10 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order("created_at DESC")
+
+  
   end
 
-
-  def show
-  end
-
-
-  def edit
-  end
 
 
   private
@@ -37,6 +31,5 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 end
-
 
 
