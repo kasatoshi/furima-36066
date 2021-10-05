393f761eea8e6490a1226bc239a82da4e92b3c60
kasatoshi <gold_experience_3104@yahoo.co.jp>
2021-10-05 15:51:37 +0900

    商品の情報編集　3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:create, :show, :index]
+  before_action :authenticate_user!, except: [:create, :show, :index, :edit]
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:index, :show, :edit]
   def new
     @item = Item.new
   end
@@ -51,9 +50,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def move_to_index
-    redirect_to new_user_session_path unless user_signed_in?
-  end
+
 end
 
 
